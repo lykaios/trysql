53723d39429cb9fe7c54c42f781ccd9553ee294e
lykaios <nateallen89@hotmail.com>
2012-02-20 14:56:31 -0800

    Played around with output methods

--- app/controllers/sqlcons_controller.rb
+++ app/controllers/sqlcons_controller.rb
@@ -12,18 +12,10 @@ class SqlconsController < ApplicationController
   #Gets query from input
   def fetchquery
     
-    qstring = params[:q]
+    @qstring = params[:q]
     #render :text => qstring
-	@qresults = ActiveRecord::Base.connection.execute(qstring)
+	@qresults = ActiveRecord::Base.connection.execute(@qstring)
+	render :show
   end
 
-  #Formats onto page
-  def show
-	respond_to do |format|
-	  format.html # show.html.erb
-	  format.json { render json: @qresults }
-	end
-end
-
-    
 end
