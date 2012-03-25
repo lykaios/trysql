2692ac5d689d02ea529433b85929e4b993322de5
lykaios <nateallen89@hotmail.com>
2012-03-24 17:03:55 -0700

    Model cleanup pt2

--- app/models/student.rb
+++
@@ -1,4 +0,0 @@
-class Student < ActiveRecord::Base
-  validates :name, :presence => true
-  validates :id,  :uniqueness => true
-end
--- app/models/teacher.rb
+++
@@ -1,4 +0,0 @@
-class Teacher < ActiveRecord::Base
-  validates :name, :presence => true
-  validates :id,  :uniqueness => true
-end
