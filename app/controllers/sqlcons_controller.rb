54dc51697d161f84c5737e2c011b163e871ed390
lykaios <nateallen89@hotmail.com>
2012-05-02 13:12:28 -0700

    Switched regex to case insensitive matching, and am only downcasing table names. Seems to be working.

--- app/controllers/sqlcons_controller.rb
+++ app/controllers/sqlcons_controller.rb
@@ -5,20 +5,20 @@ class SqlconsController < ApplicationController
   # GET /sqlcons.json
   def index
   	session[:maxsec]  = Chapters.select(:maxlesson).where("id = 1").first.maxlesson	
-  	session[:maxch]   = Chapters.maximum(:id)
   	session[:tutch]   = 1
     session[:tutsec]  = 1  
-  	respond_to do |format|
-        format.html # index.html.erb
-        format.json { render :json => @sqlcons }
-      end
-    end
+  	#respond_to do |format|
+     #   format.html # index.html.erb
+     #   format.json { render :json => @sqlcons }
+     # end
+	render :index  
+  end
 
   #Returns users query into controller from view
   #@qstring = the query passed through params
   def fetchquery
   	#Save initial query so user doesn't see uid regulation	
-  	@qstring_init = params[:q].downcase
+  	@qstring_init = params[:q].gsub(/(courses|students|course_fcts|teachers)/i) {|tab|tab.downcase}
 	#Use strip to trim out the whitespace
   	@qstring      = @qstring_init.strip
   	#Pull back regexp for the specific lesson
@@ -67,7 +67,6 @@ class SqlconsController < ApplicationController
   #Executes a given query against the chosen database
   def execquery
   	@qstatus = 0
-
   	#Fetch query results, rescue from any mysql exceptions
   	begin
   	  @qstring = append_query(@qstring) 
@@ -93,12 +92,12 @@ class SqlconsController < ApplicationController
 	  # else the results are not what user intended. 
 	  #We have to modify where we place 'where_clause' based on the incoming 
 	  # SQL statment. Otherwise we create a syntax error
-	  if p_qstring =~ /(insert)/
+	  if p_qstring =~ /(insert)/i
 		ret_string = p_qstring.gsub /\)/, ', ' + uid + ') \2'
 	  else
 		tabname  = p_qstring[/(from)( )+([_a-z]+)( )*([_a-z]*)/, 3]
 		tabalias = p_qstring[/(from)( )+([_a-z]+)( )+([_a-z]*)/, 5]
-		if p_qstring =~ /(update)/
+		if p_qstring =~ /(update)/i
 		  tabname  = p_qstring[/(update)( )+([_a-z]+)( )*([_a-z]*)/, 3]
 		  tabalias = p_qstring[/(update)( )+([_a-z]+)( )+([_a-z]*)/, 5]
 		end
@@ -109,16 +108,16 @@ class SqlconsController < ApplicationController
 		end
 		where_clause = ' where ' + table_clause
   
-		if p_qstring =~ /(right)(.*?)(join)/
-		  ret_string = p_qstring.gsub /(join)( )*([_a-z]+)( )+([_a-z]+)(.*?)(on)/, '\1 \2 \3 \4 \5 \6 \7 \5.uid = ' + uid + ' and '
-		elsif p_qstring =~ /(join)/
-		  ret_string = p_qstring.gsub /(on)(.*?)([_a-z]*)\.([_a-z]*)(.*?)([_a-z]+)\.([_a-z]+)/, '\1 \2\3.uid = \6.uid and \3.uid = '+uid+' and \3.\4 = \6.\7'
-		elsif p_qstring =~ /(where)/
-		  ret_string = p_qstring.gsub /(where)/, where_clause + ' and '
-		elsif p_qstring =~ /(group)/
-		  ret_string = p_qstring.gsub /(group)/, where_clause + ' group '
-		elsif p_qstring =~ /(order)/
-		  ret_string = p_qstring.gsub /(order)/, where_clause + ' order '
+		if p_qstring =~ /(right)(.*?)(join)/i
+		  ret_string = p_qstring.gsub /(join)( )*([_a-z]+)( )+([_a-z]+)(.*?)(on)/i, '\1 \2 \3 \4 \5 \6 \7 \5.uid = ' + uid + ' and '
+		elsif p_qstring =~ /(join)/i
+		  ret_string = p_qstring.gsub /(on)(.*?)([_a-z]*)\.([_a-z]*)(.*?)([_a-z]+)\.([_a-z]+)/i, '\1 \2\3.uid = \6.uid and \3.uid = '+uid+' and \3.\4 = \6.\7'
+		elsif p_qstring =~ /(where)/i
+		  ret_string = p_qstring.gsub /(where)/i, where_clause + ' and '
+		elsif p_qstring =~ /(group)/i
+		  ret_string = p_qstring.gsub /(group)/i, where_clause + ' group '
+		elsif p_qstring =~ /(order)/i
+		  ret_string = p_qstring.gsub /(order)/i, where_clause + ' order '
 		else
 		  ret_string = p_qstring + where_clause 
 		end
@@ -146,6 +145,4 @@ class SqlconsController < ApplicationController
   	@error_name = path + errname
 	@help_name = path + "help.html"
   end
-
-
 end
--- app/models/sqlcons.rb
+++ app/models/sqlcons.rb
@@ -3,9 +3,10 @@ class Sqlcons < ActiveRecord::Base
 
   def checkquery
 	#create a regexp object based on string we recieve from controller
-	reg = Regexp.new(regtext.to_s)
+	#true makes for case insensitive matching
+	reg = Regexp.new(regtext.to_s,true)
 	#regexp to make validate against common sqlinjection
-	reg2 = Regexp.new('(alter|drop|create|index|truncate|show|sqlcons|dbq|schema_migrations|;|union)')
+	reg2 = Regexp.new('(alter|drop|create|index|truncate|show|sqlcons|dbq|schema_migrations|;|union)',true)
 	
 	#Check if regexp was valid for lesson, and not a sqlinject attempt	
 	return (qtext =~ reg && !(qtext =~ reg2)) 
